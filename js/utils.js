3720e711810182f10a8310c435e92dfd66a7d74b
Wilson Montelo <wilson.montelo@vibedesenv.com>
2020-05-13 18:34:44 -0300

    commit

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,16 @@ $(document).ready(function () {
   
   if ($("#wpadminbar").length) { $(".menu").css({ top: 32 }) }
   
+  $(".partners-list").slick({
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 5,
+    slidesToScroll: 3
+  });
+
+  $(".contact-carrosel-single").slick();
+
   $("#cnpj").mask("99.999.999/9999-99");
   $("#telefone").mask("(99) 999999999");
   $("#telefone_op").mask("(99) 999999999");
